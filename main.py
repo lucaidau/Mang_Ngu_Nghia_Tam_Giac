5291cea653454aecd9f6d0e95e6a92408f8cad42
Đà Điểu Sồn Sồn <hanvabom@gmail.com>
2026-04-18 22:56:37 +0700

    update_dothi

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import tkinter as tk
-from app.models.quanLiDoThi import QuanLiDoThi
 from app.views.giaoDienChinh import GiaoDienChinh
 from app.controllers.controller import Controller
 
 
 def main():
     root = tk.Tk()
-    app_model = QuanLiDoThi()
+    root.title("Phần mềm Giải Toán Tam Giác")
 
+    # 1. Khởi tạo View trước
     app_view = GiaoDienChinh(root, controller=None)
-
+    
+    # 2. Khởi tạo Controller (Model đã được tạo bên trong Controller rồi)
     app_controller = Controller(view=app_view)
+    
+    # 3. Kết nối ngược lại cho View
     app_view.controller = app_controller
-    app_controller.qldt = app_model
-
+    
+    # 4. Chạy ứng dụng
     root.mainloop()
 
 if __name__ == "__main__":
